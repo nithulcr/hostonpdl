aee5781bba33ffbacd7362e5d239d7294140d6e9
nithul <nithulcr@gmail.com>
2026-04-07 08:42:47 +0530

    dewew

--- src/app/ContactUs/page.tsx
+++ src/app/ContactUs/page.tsx
@@ -42,8 +42,16 @@ export default function ContactUsPage() {
           </motion.div>
         </div>
       </section>
+      
 
       <ContactUs />
+      <section className="w-full h-[400px] overflow-hidden">
+        
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3904.466028299119!2d75.36874957453098!3d11.872602838020189!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba43d3396df5c8d%3A0x1b182dd264f45d3e!2sHost%20On%20Pdl.Com!5e0!3m2!1sen!2sin!4v1775531501254!5m2!1sen!2sin"  width="100%"
+          height="100%"
+          style={{ border: 0 }}
+          loading="lazy" ></iframe>
+      </section>
       <Footer />
     </>
   );
